ff9be300b44fb9cb923c5799739712d5329999f4
Pavol Orem <85439920+pavolorem@users.noreply.github.com>
2024-11-18 11:12:24 +0100

    Create swap_usage.sh

---
+++ bash/swap_usage.sh
@@ -0,0 +1,5 @@
+# listing all process swap space usage desc sort out
+
+#!/bin/bash
+
+for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | less
